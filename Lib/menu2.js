210fd8700acdecb5aad6fbe1f9182ed9a698de24
Matrix <76190284+Matrix1246@users.noreply.github.com>
2020-12-24 18:41:31 -0300

    Add files via upload

---
+++ Lib/menu2.js
@@ -0,0 +1,18 @@
+exports.menu2 = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+Neno
+❉──────────❉
+
+ 🤠🤙 𝐎𝐋𝐇𝐄 𝐎𝐒 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐀𝐁𝐀𝐈𝐗𝐎
+	
+╔════════════════════
+║ *MENU2*    
+╠════════════════════
+EM REFORMA,ESTAMOS TRABALHANDO NISSO
+║╰──────────
+║ *BOT Matrix*
+╠════════════════════
+║         🤠🤙
+╚════════════════════`
+}
